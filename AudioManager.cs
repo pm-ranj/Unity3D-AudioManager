5db32235558c455f5d4526b21c275e271a689aca
payam-ranjbar <payam.zigma@gmail.com>
2019-07-04 01:13:04 +0430

    init

---
+++ AudioManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using Random = UnityEngine.Random;
+
+
+public class AudioManager :MonoBehaviour
+{
+
+	public Sound[] sounds;
+
+	private void Awake()
+	{
+		foreach (var s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+		}  
+	}
+
+	public void Play(String name)
+	{
+		Sound s = Array.Find(sounds, sounds => sounds.name == name);
+		s.source.pitch = s.pitch;
+
+		s.source.Play();
+	}
+
+	public void Play(Sound sound)
+	{
+		sound.source.pitch = sound.pitch;
+
+		sound.source.Play();
+		
+	}
+
+	public void PlayWithExtraAudioSource(string name)
+	{
+		Sound s = Array.Find(sounds, sounds => sounds.name == name);
+		s.source = gameObject.AddComponent<AudioSource>();
+		s.source.clip = s.clip;
+		s.source.volume = s.volume;
+		s.source.pitch = s.pitch;
+		s.source.Play();
+		StartCoroutine(deleteSource(s.source, s.clip.length));
+
+	}
+
+	public void PlayRandomPitch(string name, float min, float max)
+	{
+		Sound s = Array.Find(sounds, sounds => sounds.name == name);
+		s.source.pitch = s.pitch;
+
+		float range = Random.Range(min, max);
+		s.source.pitch += range;
+		s.source.Play();
+
+
+	}
+
+	public void Stop(AudioSource audioSource = null)
+	{
+		if (audioSource != null)
+		{
+			audioSource.Stop();
+		}
+
+	}
+	
+	public void Stop(String name)
+	{
+		Sound s = Array.Find(sounds, sounds => sounds.name == name);
+		s.source.pitch = s.pitch;
+		if (s.source.isPlaying)
+			s.source.Stop();
+
+	}
+
+	IEnumerator deleteSource(AudioSource s, float length)
+	{
+		yield return new WaitForSeconds(length);
+		Destroy(s);
+	}
+
+}
---
+++ Sound.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+[System.Serializable]
+public class Sound
+{
+
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 4f)]
+    public float pitch;
+
+    public String name;
+
+    [HideInInspector]
+    public AudioSource source;
+
+}
